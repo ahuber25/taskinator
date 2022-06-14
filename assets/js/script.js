7f53a8ddd164e85292a4903233527add2286c2e8
Acadia Huber <horsinaround235@gmail.com>
2022-06-14 12:53:05 -0400

    made the button clickable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.dir(window.document)
+var formE1 = document.querySelector("#task-form");
+var tasksToDoE1 = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task";
+    tasksToDoE1.appendChild(listItemEl);
+};
+
+formE1.addEventListener("submit", createTaskHandler);
